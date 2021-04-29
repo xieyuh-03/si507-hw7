ffc8f2c2022ed521d0b8256735950834f5f0f4ff
xieyuh-03 <xieyuh@umich.edu>
2021-04-29 15:46:50 +0800

    Add headlines

--- top_headlines.py
+++ top_headlines.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+import requests
+import secrets
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -23,6 +26,25 @@ def about():
 def hello_name(nm):
 
     return render_template('name.html', name=nm)
+
+@app.route('/headlines/<nm>')
+def tech_headlines(nm):
+    mykey = secrets.api_key
+    url = f'https://api.nytimes.com/svc/topstories/v2/technology.json?api-key={mykey}'
+    
+    results = requests.get(url).json()
+    headlines = []
+    i = 1
+    for item in results['results']:
+        if i < 6:
+            title = item['title']
+            headlines.append(title)
+            i += 1
+    
+    return render_template('headlines.html', name=nm, title_list=headlines)
+
+
+    
 
 if __name__ == '__main__':
     print('starting Flask app', app.name)
